d14113ae942751b31623979bbe2cf30ccbf53dbd
Anjishnu Banerjee <inviktor27@gmail.com>
2025-04-07 19:04:14 +0530

    Finish navbar

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,65 @@
 const passport = require("./passport");
 const { checkUserAuthentication } = require("./utils");
 const { body, validationResult } = require("express-validator");
+const db = require("../prisma/prisma");
+const bcrypt = require("bcryptjs");
 
-const validateSignUp = [body("username"), body("password"), body("confirm")];
+const validateSignUp = [
+  body("username")
+    .trim()
+    .isAlphanumeric()
+    .withMessage("Username must be alphanumeric.")
+    .isLength({ min: 3, max: 20 })
+    .withMessage("Username must be 3-20 characters.")
+    .custom(async (value) => {
+      const exists = await db.user.findFirst({
+        where: { username: value },
+      });
+      if (exists) throw new Error("Username already exists.");
+      return true;
+    }),
+  body("password")
+    .isLength({ min: 1, max: 20 })
+    .withMessage("Password must be 8-20 characters"),
+  body("confirm")
+    .custom((value, { req }) => value === req.body.password)
+    .withMessage("Confirm password doesn't match"),
+];
 
 exports.userLogInGet = (req, res) => {
-  res.render("logIn", { title: "Log in" });
+  let errors = null;
+  if (req.session.messages) {
+    errors = req.session.messages.map((error) => ({ msg: error }));
+    delete req.session.messages;
+  }
+  res.render("logIn", { title: "Log in", errors });
 };
+
 exports.userSignUpGet = (req, res) => {
   res.render("signUp", { title: "Sign up" });
 };
 
+exports.userSignUpPost = [
+  validateSignUp,
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.render("signUp", { title: "Sign up", errors: errors.array() });
+    }
+    const { username, password } = req.body;
+    await db.user.create({
+      data: {
+        username,
+        password: await bcrypt.hash(password, 10),
+      },
+    });
+    res.redirect("/log-in");
+  },
+];
+
 exports.showRoot = [
   checkUserAuthentication,
-  (req, res) => res.render("files", { title: "Your Files" }),
+  (req, res) => res.render("index", { title: "Your Files" }),
 ];
 
 exports.userLogOut = (req, res, next) => {
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,10 @@
 const userController = require("../controllers/userController");
 const { Router } = require("express");
+const { setUser } = require("../controllers/utils");
 
 const userRouter = new Router();
+
+userRouter.use(setUser);
 
 userRouter.get("/", userController.showRoot);
 userRouter.get("/log-out", userController.userLogOut);
@@ -10,6 +13,6 @@ userRouter.get("/log-in", userController.userLogInGet);
 userRouter.post("/log-in", userController.userLogInPost);
 
 userRouter.get("/sign-up", userController.userSignUpGet);
-// userRouter.post("/sign-up", userController.userSignUpPost);
+userRouter.post("/sign-up", userController.userSignUpPost);
 
 module.exports = userRouter;
